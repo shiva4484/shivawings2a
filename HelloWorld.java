da90d0d796897a91ad177ead27d266b55a354b9c
shiva4484 <shivakanth.av@gmail.com>
2022-01-07 12:17:12 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,7 @@
-public class HelloWorld {
-  public static void main(String[] args) {
-    System.out.println("Hello world git webhooks");
-  }
+public class HelloWorld
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
